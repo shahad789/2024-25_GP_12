e1a611932934dbe5174663c1c11ad1ced32f171c
shahad789 <t.shahad12345@outlook.com>
2025-01-26 12:36:02 +0300

    favorite finish

--- lib/widgets/house_fav.dart
+++ lib/widgets/house_fav.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:daar/screens/fav.dart';
+import 'package:daar/usprovider/UserProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 class ItemCard extends StatefulWidget {
   const ItemCard(this.item, this.onTap, {super.key});
@@ -20,8 +23,40 @@ class _ItemCardState extends State<ItemCard> {
         RegExp(r'(\d)(?=(\d{3})+(?!\d))'), (Match match) => '${match[1]},');
   }
 
+  Future<void> _toggleFavorite(String? currentUserId) async {
+    if (currentUserId == null) return;
+
+    final userRef =
+        FirebaseFirestore.instance.collection('user').doc(currentUserId);
+    final userDoc = await userRef.get();
+
+    List<String> favorites = [];
+
+    if (userDoc.exists && userDoc.data() != null) {
+      // Ensure 'favorites' exists and is a List<String>
+      favorites = List<String>.from(userDoc.data()?['favorites'] ?? []);
+    }
+
+    if (favorites.contains(widget.item.id)) {
+      favorites.remove(widget.item.id);
+    } else {
+      favorites.add(widget.item.id);
+    }
+
+    // Update Firestore with merged data
+    await userRef.set({'favorites': favorites}, SetOptions(merge: true));
+
+    setState(() {
+      isFavorite = favorites.contains(widget.item.id);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // Access user ID from UserProvider
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    final currentUserId = userProvider.userDocId;
+
     final priceFormatted =
         NumberFormat('#,##0', 'en_US').format(widget.item.price);
 
@@ -57,22 +92,20 @@ class _ItemCardState extends State<ItemCard> {
                 ),
                 const SizedBox(height: 10.0),
 
-                //type
+                //type, size, and favorite icon
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Text(
-                      widget.item.category,
-                      style: const TextStyle(
-                        color: Color.fromARGB(255, 0, 0, 0),
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-
-                    //size
+                    // Size and Favorite Icon on the left
                     Row(
                       children: [
+                        IconButton(
+                          icon: Icon(
+                            Icons.favorite,
+                            color: isFavorite ? Colors.red : Colors.grey,
+                          ),
+                          onPressed: () => _toggleFavorite(currentUserId),
+                        ),
                         Text(
                           '${widget.item.size} م²',
                           style: const TextStyle(
@@ -81,18 +114,17 @@ class _ItemCardState extends State<ItemCard> {
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        IconButton(
-                          icon: Icon(
-                            Icons.favorite,
-                            color: isFavorite ? Colors.red : Colors.white,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              isFavorite = !isFavorite;
-                            });
-                          },
-                        ),
                       ],
+                    ),
+
+                    // Category on the right
+                    Text(
+                      widget.item.category,
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0),
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ],
                 ),
@@ -113,11 +145,8 @@ class _ItemCardState extends State<ItemCard> {
 
                 //city district
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    const Icon(
-                      Icons.location_on,
-                      color: Color.fromARGB(255, 42, 19, 117),
-                    ),
                     Text(
                       '${widget.item.city} حي ${widget.item.district}',
                       style: const TextStyle(
@@ -127,18 +156,25 @@ class _ItemCardState extends State<ItemCard> {
                       ),
                       overflow: TextOverflow.ellipsis,
                     ),
+                    const Icon(
+                      Icons.location_on,
+                      color: Color.fromARGB(255, 42, 19, 117),
+                    ),
                   ],
                 ),
                 const SizedBox(height: 20.0),
 
                 //price
-                Text(
-                  '$priceFormatted ريال سعودي',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 19.0,
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    '$priceFormatted ريال سعودي',
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 19.0,
+                    ),
+                    overflow: TextOverflow.ellipsis,
                   ),
-                  overflow: TextOverflow.ellipsis,
                 ),
                 const SizedBox(height: 8.0),
               ],
