b61699b8733c668cd939ddfdf02616e9b864de84
Sarah <sos024@outlook.sa>
2025-01-26 19:26:36 +0300

    add notification

--- lib/screens/notif.dart
+++ lib/screens/notif.dart
@@ -1,7 +1,8 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:daar/usprovider/UserProvider.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
+import 'package:daar/usprovider/UserProvider.dart';
 
 class NotifPage extends StatefulWidget {
   const NotifPage({Key? key}) : super(key: key);
@@ -11,7 +12,7 @@ class NotifPage extends StatefulWidget {
 }
 
 class _NotifPageState extends State<NotifPage> {
-  List<QueryDocumentSnapshot> properties = []; // تخزين البيانات محليًا
+  List<QueryDocumentSnapshot> properties = [];
 
   @override
   Widget build(BuildContext context) {
@@ -75,81 +76,109 @@ class _NotifPageState extends State<NotifPage> {
                   ),
                   color: Colors.white,
                 ),
-                child: Builder(
-                  builder: (context) {
-                    // Check if today is Friday
-                    if (DateTime.now().weekday != DateTime.sunday) {
+                child: StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('Property')
+                      .where('user',
+                          isEqualTo:
+                              FirebaseFirestore.instance.doc('user/$userDocId'))
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
                       return const Center(
-                        child: Text('لا توجد إشعارات لليوم.'),
+                        child: CircularProgressIndicator(),
                       );
-                    }
+                    } else if (snapshot.hasError) {
+                      print('Error fetching data: ${snapshot.error}');
+                      return const Center(
+                        child: Text('حدث خطأ أثناء تحميل البيانات.'),
+                      );
+                    } else if (!snapshot.hasData ||
+                        snapshot.data!.docs.isEmpty) {
+                      print('No notifications found for user: $userDocId');
+                      return const Center(child: Text('لا توجد إشعارات.'));
+                    } else {
+                      properties = snapshot.data!.docs;
 
-                    // If it's Friday, proceed to display notifications
-                    return StreamBuilder<QuerySnapshot>(
-                      stream: FirebaseFirestore.instance
-                          .collection('Property')
-                          .where('user',
-                              isEqualTo: FirebaseFirestore.instance
-                                  .doc('user/$userDocId'))
-                          .snapshots(),
-                      builder: (context, snapshot) {
-                        if (snapshot.connectionState ==
-                            ConnectionState.waiting) {
-                          return const Center(
-                              child: CircularProgressIndicator());
-                        } else if (snapshot.hasError) {
-                          return const Center(
-                              child: Text('حدث خطأ أثناء تحميل البيانات.'));
-                        } else if (!snapshot.hasData ||
-                            snapshot.data!.docs.isEmpty) {
-                          return const Center(child: Text('لا توجد إشعارات.'));
-                        } else {
-                          properties = snapshot.data!.docs;
+                      return ListView.builder(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 18.0, vertical: 30.0),
+                        itemCount: properties.length,
+                        itemBuilder: (context, index) {
+                          final doc = properties[index];
+                          final propertyData =
+                              doc.data() as Map<String, dynamic>;
+
+                          final propertyDistrict =
+                              propertyData['District'] ?? 'الحي';
+                          final propertyCity =
+                              propertyData['city'] ?? 'المدينة غير محددة';
+                          final propertySize =
+                              propertyData['size'] ?? 'غير محدد';
+                          final propertyView = propertyData['view'] ?? 0;
+
+                          final dateListed = propertyData['Date_list'] != null
+                              ? (propertyData['Date_list'] as Timestamp)
+                                  .toDate()
+                              : null;
+
+                          final notifications = <Widget>[];
+
+                          // Add notification for views
+                          if (propertyView > 0) {
+                            final viewNotificationMessage =
+                                'تمت مشاهدة عقارك في $propertyCity، حي $propertyDistrict، الذي حجمه $propertySize من قبل $propertyView شخص';
+                            notifications.add(
+                              NotificationCard(
+                                title: 'مشاهدات عقارك',
+                                message: viewNotificationMessage,
+                              ),
+                            );
+                          }
+
+                          // Add notification for date
+                          if (dateListed != null) {
+                            final daysListed =
+                                DateTime.now().difference(dateListed).inDays;
+
+                            final dateNotificationMessage =
+                                'تم إدراج عقارك في $propertyCity، حي $propertyDistrict منذ $daysListed يومًا. هل لديك تحديثات للعقار مثل تعديل السعر أو التفاصيل؟ قم بتحديث إعلانك الآن لجذب المزيد من المشترين!';
+                            notifications.add(
+                              NotificationCard(
+                                title: 'حدث حاله عقارك',
+                                message: dateNotificationMessage,
+                              ),
+                            );
+                          }
+
                           return Column(
                             children: [
-                              Expanded(
-                                child: ListView.builder(
-                                  padding: const EdgeInsets.symmetric(
-                                      horizontal: 18.0, vertical: 30.0),
-                                  itemCount: properties.length,
-                                  itemBuilder: (context, index) {
-                                    final doc = properties[index];
-                                    final propertyData =
-                                        doc.data() as Map<String, dynamic>;
-
-                                    final propertyDistrict =
-                                        propertyData['District'] ?? 'الحي';
-                                    final propertyCity = propertyData['city'] ??
-                                        'المدينة غير محددة';
-                                    final propertySize =
-                                        propertyData['size'] ?? 'غير محدد';
-                                    final propertyView =
-                                        propertyData['view'] ?? 0;
-
-                                    final title = 'مشاهدات عقارك';
-                                    final message =
-                                        'تمت مشاهدة عقارك في $propertyCity، حي $propertyDistrict، الذي حجمه $propertySize من قبل $propertyView شخص';
-
-                                    return Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.end,
-                                      children: [
-                                        NotificationCard(
-                                          title: title,
-                                          message: message,
-                                        ),
-                                        const Divider(
-                                            height: 20, color: Colors.grey),
-                                      ],
-                                    );
-                                  },
+                              for (int i = 0;
+                                  i < notifications.length;
+                                  i++) ...[
+                                notifications[i],
+                                if (i < notifications.length - 1)
+                                  const Divider(
+                                    height: 20,
+                                    color: Colors.grey,
+                                    thickness: 1.0,
+                                    indent: 16.0,
+                                    endIndent: 16.0,
+                                  ),
+                              ],
+                              if (index < properties.length - 1)
+                                const Divider(
+                                  height: 30,
+                                  color: Colors.black26,
+                                  thickness: 1.0,
+                                  indent: 10.0,
+                                  endIndent: 10.0,
                                 ),
-                              ),
                             ],
                           );
-                        }
-                      },
-                    );
+                        },
+                      );
+                    }
                   },
                 ),
               ),
@@ -173,27 +202,42 @@ class NotificationCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        Text(
-          title,
-          textAlign: TextAlign.right,
-          style: const TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+    return Container(
+      margin: const EdgeInsets.only(bottom: 16.0),
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.blue.shade50,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 8,
+            offset: const Offset(0, 4),
           ),
-        ),
-        const SizedBox(height: 8.0),
-        Text(
-          message,
-          textAlign: TextAlign.right,
-          style: const TextStyle(
-            fontSize: 16,
-            color: Colors.black54,
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            title,
+            textAlign: TextAlign.right,
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-      ],
+          const SizedBox(height: 8.0),
+          Text(
+            message,
+            textAlign: TextAlign.right,
+            style: const TextStyle(
+              fontSize: 16,
+              color: Colors.black54,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
